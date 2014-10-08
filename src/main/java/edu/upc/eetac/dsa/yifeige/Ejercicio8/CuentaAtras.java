ec80c3724de01b5553736042263c4faa4ae0cdcf
yifei ge <yokihappy@hotmail.com>
2014-10-08 18:28:10 +0200

    Ejercicio 10y11 añadido

--- src/main/java/edu/upc/eetac/dsa/yifeige/Ejercicio8/CuentaAtras.java
+++ src/main/java/edu/upc/eetac/dsa/yifeige/Ejercicio8/CuentaAtras.java
@@ -3,30 +3,41 @@ package edu.upc.eetac.dsa.yifeige.Ejercicio8;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class CuentaAtras extends Thread {
+public class CuentaAtras extends Thread 
+{
 	String s;
+	private static String  ultimo;// ejercicio 10
 	int numero;
 	int segundo = 0;
 
-	public CuentaAtras(String id, int num) {
+	public CuentaAtras(String id, int num) 
+	{
 		this.s = id;
 		this.numero = num;
 
 	}
 
-	public void run() {
+	public void run() 
+	{
 
-		while (numero >= 0) {
-			System.out.printf("El %s su numero actual es %d\n", s, numero);
+		while (numero >= 0) 
+		{
+			synchronized(this) //ejercicio 11
+			{
+			this.ultimo=s; // ejercicio 10
+			System.out.printf("El thread %s su numero actual es %d,la ultima escritura por la pantalla es: %s\n", s, numero,this.ultimo);
 			numero = numero - 1;
-			try {
+			}
+			try 
+			{
 
 				this.sleep(1 * 1000);
 				segundo = segundo + 1;
 				System.out.printf("Para el thread %s Ha pasado %d segundos\n",
 						s, segundo);
 
-			} catch (InterruptedException e) {
+			} catch (InterruptedException e) 
+			{
 				e.printStackTrace();
 			}
 		}
